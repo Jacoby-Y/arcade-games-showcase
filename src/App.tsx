b427faafd03c47cebebea9a1b22332e2b796d112
Jacoby-Y <cobyyliniemi@gmail.com>
2023-04-12 18:58:41 -0500

    Started the carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+// import { Button } from "react-bootstrap";
+import Carousel from 'react-bootstrap/Carousel';
+
 function App() {
   return (
     <div id="App">
-      Arcade games!
+      <p>Arcade games!</p>
+      <Carousel interval={null}>
+        <Carousel.Item>
+          <h3>Game #1</h3>
+          <Carousel.Caption>
+            <p>Pong</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <h3>Game #2</h3>
+          <Carousel.Caption>
+            <p>Snake</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <h3>Game #3</h3>
+          <Carousel.Caption>
+            <p>Tetris</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
     </div>
   )
 }
